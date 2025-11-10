7415b1997397a556ebab1a29f994652ffc0bbdc8
Myles Browne <myles@browne.link>
2025-11-10 17:49:42 +0000

    updates ball options

--- discordApi/slashCommands.go
+++ discordApi/slashCommands.go
@@ -145,30 +145,49 @@ func (d Discord) processWheelCommand(interaction *discordgo.InteractionCreate) {
 }
 
 func (d Discord) process8BallCommand(interaction *discordgo.InteractionCreate) {
+	ballAnswers := []string{
+		"It is certain",
+		"It is decidedly so",
+		"Without a doubt",
+		"Yes definitely",
+		"You may rely on it",
+		"As the ball sees it, yes",
+		"Most likely",
+		"Outlook good",
+		"Yes",
+		"Signs point to yes",
+		"Reply hazy, try again",
+		"Ask again later",
+		"Better not tell you now",
+		"Cannot predict now",
+		"Concentrate and ask again",
+		"Don't count on it",
+		"It's reply is no",
+		"It's sources say no",
+		"Outlook not so good",
+		"Very doubtful"}
 
-	data := interaction.ApplicationCommandData()
 	session := d.Session
-	user := interaction.Member.Nick
-
+	data := interaction.ApplicationCommandData()
 	question := data.Options[0].StringValue()
-	ballAnswers := []string{"Yes", "No"}
+	user := interaction.Member.Nick
 
 	if len(question) == 0 {
 		session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{
-				Content: fmt.Sprintf("sorry, %s, but i cant seem to find your question :(", user),
+				Content: fmt.Sprintf("sorry, %s, but the ball refuses to answer my call :(", user),
 			},
 		})
 
 	}
 
-	chosen := ballAnswers[rand.Intn(len(ballAnswers))]
+	selectedAnswer := ballAnswers[rand.Intn(len(ballAnswers))]
 
 	session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
-			Content: fmt.Sprintf("I asked the magical 8 ball \"%s\" , and it said **%s**.", question, chosen),
+			Content: fmt.Sprintf("I asked the magical 8 ball \"%s\" , and it said **%s**.", question, selectedAnswer),
 		},
 	})
 }
